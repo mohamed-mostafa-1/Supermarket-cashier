78092a889605b8e6b615e1ffa877b3253d447241
Mohamed Mostafa <mohamedmostafa23104@gmail.com>
2025-04-06 20:20:12 +0200

    Add project files.

---
+++ C#.consoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C_.consoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcomeing = "Hello, World!";
+            Console.WriteLine(welcomeing);
+        }
+    }
+}
